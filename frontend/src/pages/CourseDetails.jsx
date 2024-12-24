46e25a427ef6d729de498d00ca08e866cbab4692
Amankumarjha15 <amanjhakr6299@gmail.com>
2024-12-24 23:05:15 +0530

    course detail page updated

--- frontend/src/pages/CourseDetails.jsx
+++ frontend/src/pages/CourseDetails.jsx
@@ -6,7 +6,6 @@ import { useDispatch, useSelector } from "react-redux"
 import { useNavigate, useParams } from "react-router-dom"
 
 import ConfirmationModal from "../components/common/ConfirmationModal"
-import Footer from "../components/common/Footer"
 import RatingStars from "../components/common/RatingStars"
 import CourseAccordionBar from "../components/Course/CourseAccordionBar"
 import CourseDetailsCard from "../components/Course/CourseDetailsCard"
