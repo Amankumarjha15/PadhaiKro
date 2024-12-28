1ee0fa01233181e107e6054805ca760b6dd4a8b6
Amankumarjha15 <amanjhakr6299@gmail.com>
2024-12-28 19:18:39 +0530

    some design issue solved

--- frontend/src/components/common/ReviewSlider.jsx
+++ frontend/src/components/common/ReviewSlider.jsx
@@ -7,7 +7,7 @@ import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/css"
 import "swiper/css/free-mode"
 import "swiper/css/pagination"
-import "../../App.css"
+import "../../index.css"
 // Icons
 import { FaStar } from "react-icons/fa"
 // Import required modules
@@ -33,7 +33,7 @@ function ReviewSlider() {
     })()
   }, [])
 
-  // console.log(reviews)
+  console.log(reviews)
 
   return (
     <div className="text-white">
@@ -44,7 +44,7 @@ function ReviewSlider() {
           loop={true}
           freeMode={true}
           autoplay={{
-            delay: 2500,
+            delay: 1000,
             disableOnInteraction: false,
           }}
           modules={[FreeMode, Pagination, Autoplay]}
@@ -53,17 +53,20 @@ function ReviewSlider() {
           {reviews.map((review, i) => {
             return (
               <SwiperSlide key={i}>
-                <div className="flex flex-col gap-3 bg-richblack-800 p-3 text-[14px] text-richblack-25">
+                <div className="w-full flex flex-col gap-3 bg-richblack-800 p-3 text-[14px] text-richblack-25">
                   <div className="flex items-center gap-4">
-                    <img
-                      src={
+                    {/* <img
+                      className="h-9 w-9 rounded-full object-cover"
+                      src={review?.user?.image}
+                      alt=""
+                    /> */}
+                    <div className="h-9 w-9 rounded-full object-cover">
+                    <img src=  {
                         review?.user?.image
                           ? review?.user?.image
                           : `https://api.dicebear.com/5.x/initials/svg?seed=${review?.user?.firstName} ${review?.user?.lastName}`
-                      }
-                      alt=""
-                      className="h-9 w-9 rounded-full object-cover"
-                    />
+                      } alt="user image"  className="rounded-full" />
+                    </div>
                     <div className="flex flex-col">
                       <h1 className="font-semibold text-richblack-5">{`${review?.user?.firstName} ${review?.user?.lastName}`}</h1>
                       <h2 className="text-[12px] font-medium text-richblack-500">
@@ -97,7 +100,6 @@ function ReviewSlider() {
               </SwiperSlide>
             )
           })}
-          {/* <SwiperSlide>Slide 1</SwiperSlide> */}
         </Swiper>
       </div>
     </div>
