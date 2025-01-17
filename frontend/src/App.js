7a1dd3a75e10dc942ced2bd4ad254d7803e37dff
Amankumarjha15 <amanjhakr6299@gmail.com>
2025-01-17 21:13:17 +0530

    production error changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { Route , Routes, useNavigate } from "react-router-dom";
-import Home from './pages/home'
+import Home from './pages/Home';
 import Navbar from "./components/common/Navbar";
 import OpenRoute from "./components/Auth/OpenRoute";
 import Login from "./pages/Login";
